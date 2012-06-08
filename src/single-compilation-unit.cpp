64074e170319e818b5db0f2cbe003508c698cbf7
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2012-06-08 14:58:23 +0000

    checked in missing file
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/module-ssh2/trunk@4915 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ src/single-compilation-unit.cpp
@@ -0,0 +1,8 @@
+#include "QC_SSH2Base.cpp"
+#include "QC_SSH2Client.cpp"
+#include "QC_SSH2Channel.cpp"
+#include "QC_SFTPClient.cpp"
+#include "SSH2Client.cpp"
+#include "SFTPClient.cpp"
+#include "SSH2Channel.cpp"
+#include "ssh2-module.cpp"
