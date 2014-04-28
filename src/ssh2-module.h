a1590b96288320fd3358cadb0f42057548494b39
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2014-04-28 09:22:29 +0000

    fixed a typo in the Queue support
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/module-ssh2/trunk@6445 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/ssh2-module.h
+++ src/ssh2-module.h
@@ -4,6 +4,7 @@
   SSH2/SFTP integration to QORE
 
   Copyright 2009 Wolfgang Ritzinger
+  Copyright (C) 2010 - 2014 Qore Technologies, sro
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
